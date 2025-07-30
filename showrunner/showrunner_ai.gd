88183eda9f0e1eab1b44ed0e76a113be6962fbc9
Mikhail Schmalzried <mikhail@photonable.com>
2025-07-30 12:17:42 -0600

    Refactor file structure and remove test assets
    Standardized file and folder naming to lowercase for consistency across resources, scripts, and scenes. Removed test script and video assets no longer needed. Updated scene references and project settings to match new paths. Moved ShowrunnerAI assets to 'showrunner' directory.

---
+++ showrunner/showrunner_ai.gd
@@ -0,0 +1,8 @@
+class_name ShowRunner extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Maestro.showrunner_ai = self
+
+func _process(delta: float) -> void:
+	pass
